c7adfd08716fdd006ace1ad8ff634597a35556f1
pos-graduacao-ifsp-caio <morcelicaio2@gmail.com>
2019-04-09 23:00:27 -0300

    New commit

--- app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/retrofit/Tradutor.kt
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/retrofit/Tradutor.kt
@@ -1,2 +1,100 @@
 package br.edu.ifsp.scl.tradutorsdmkt.retrofit
 
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.APP_ID_FIELD
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.APP_ID_VALUE
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.APP_KEY_FIELD
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.APP_KEY_VALUE
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.URL_BASE
+import br.edu.ifsp.scl.tradutorsdmkt.MainActivity
+import br.edu.ifsp.scl.tradutorsdmkt.model.Resposta
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.ResponseBody
+import org.jetbrains.anko.design.snackbar
+import org.json.JSONException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+class Tradutor(val mainActivity: MainActivity) {
+
+    // Cliente Http que conterá o interceptador de requisição para cabeçalhos dinâmicos
+    val okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+
+
+    // Instanciando o cliente HTTP
+    init {
+        // Adiciona um interceptador que é um objeto de uma classe anônima
+        okHttpClientBuilder.addInterceptor(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+                // Resgatando requisição interceptada
+                val reqInterceptada: Request = chain.request()
+                // Criando nova requisição a partir da interceptada e adicionando campos de cabeçalho
+                val novaReq: Request = reqInterceptada.newBuilder()
+                    .header(APP_ID_FIELD, APP_ID_VALUE)
+                    .header(APP_KEY_FIELD, APP_KEY_VALUE)
+                    .method(reqInterceptada.method(), reqInterceptada.body())
+                    .build()
+                // retornando a nova requisição preenchdia
+                return chain.proceed(novaReq)
+            }
+        })
+    }
+    // Novo objeto Retrofit usando a URL base e o HttpClient com interceptador
+    val retrofit: Retrofit = Retrofit.Builder().baseUrl(URL_BASE).client(okHttpClientBuilder.build()).build()
+
+    // Cria um objeto Retrofit usando a URL base.
+    // val retrofit: Retrofit = Retrofit.Builder().baseUrl(URL_BASE).build()
+    // Cria um objeto, a partir da Interface Retrofit, que contém as funções de requisição
+    val tradutorApi: TradutorApi = retrofit.create(TradutorApi::class.java)
+
+
+    fun traduzir(palavraOrigem: String, idiomaOrigem: String, idiomaDestino: String) {
+        /*Chama a função de requisição definida na Interface passando os parâmetros escolhidos pelo usuário e
+    enfileira a requisição que recebe um objeto de uma implementação anônima de Callback<ResponseBody>*/
+        tradutorApi.getTraducaoByPath(idiomaOrigem, palavraOrigem, idiomaDestino).enqueue(
+            object : Callback<ResponseBody> {
+                // Função chamada no caso de erro
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    mainActivity.mainLl.snackbar("Erro na resposta - Retrofit")
+                }
+
+                // Função chamada no caso de resposta
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                    try {
+                        // Cria um objeto Gson que consegue fazer reflexão de um Json para Data Class
+                        val gson: Gson = Gson()
+                        // Reflete a resposta (que é um Json) num objeto da classe Resposta
+                        val resposta: Resposta = gson.fromJson(response.body()?.string(), Resposta::class.java)
+                        // StringBuffer para armazenar o resultado das traduções
+                        var traduzidoSb = StringBuffer()
+                        // Parseando o objeto e adicionando as traduções ao StringBuffer O(N^5)
+                        resposta.results?.forEach {
+                            it?.lexicalEntries?.forEach {
+                                it?.entries?.forEach {
+                                    it?.senses?.forEach {
+                                        it?.translations?.forEach {
+                                            traduzidoSb.append("${it?.text}, ")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        // Enviando as tradução ao Handler da thread de UI para serem mostrados na tela
+                        mainActivity.tradutoHandler.obtainMessage(
+                            MainActivity.codigosMensagen.RESPOSTA_TRADUCAO,
+                            traduzidoSb.toString().substringBeforeLast(',')
+                        ).sendToTarget()
+                    } catch (jse: JSONException) {
+                        mainActivity.mainLl.snackbar("Erro na resposta - Retrofit")
+                    }
+                }
+            } // Fim da classe anônima
+        ) // Fim dos parâmetros de enqueue
+    } // Fim da função traduzir
+
+}
--- app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/retrofit/TradutorApi.kt
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/retrofit/TradutorApi.kt
@@ -1,2 +1,25 @@
 package br.edu.ifsp.scl.tradutorsdmkt.retrofit
 
+import br.edu.ifsp.scl.tradutorsdmkt.Constantes.APP_ID_VALUE
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface TradutorApi {
+    /* Com campos de cabeçalho estáticos, é possível defini-los na Interface antes da função de requisição */
+    /*@Headers(
+        "app_id: ee2cf346", // Preencha com seu app_id
+        "app_key: 2b2687af1e633644e7b8c4488a3c56f9" // Preencha com seu app_key
+    )       */
+    /* Função de requisição. Começa com uma anotação denotando o método HTTP e endpoint da requisição
+    As anotações dos parâmetros da função descrevem o mapeamento para parâmetros do Path da requisição. */
+    @GET("entries/{source_translation_language}/{word_id}/translations={target_translation_language}")
+    fun getTraducaoByPath(
+        @Path("source_translation_language") stl: String,// stl -> source_translation_language
+        @Path("word_id") wId: String, // wId -> word_id
+        @Path("target_translation_language") ttl: String // ttl -> target_translation_language
+    ): Call<ResponseBody>
+    // Outras funções de requisição poderiam ser colocadas aqui inclusive para outros métodos.
+}
